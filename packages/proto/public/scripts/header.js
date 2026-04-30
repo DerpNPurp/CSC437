cba22b9a6ea7d5977683e488fb38a99fb7bae56c
DerpNPurp <vincedoanschool@gmail.com>
2026-04-29 22:34:29 -0700

    centralize header in header.js, restore Game Database h1, fix dark mode placement

--- packages/proto/public/scripts/header.js
+++ packages/proto/public/scripts/header.js
@@ -1,7 +1,15 @@
-const label = document.querySelector('header label');
+const header = document.querySelector('header');
+
+header.innerHTML = `
+  <h1><a href="/index.html">Game Database</a></h1>
+  <label>
+    <input type="checkbox" autocomplete="off" />
+    Dark mode
+  </label>
+`;
 
 // relay the checkbox change event as a custom event on the body
-label.onchange = function(event) {
+header.querySelector('label').onchange = function(event) {
   event.stopPropagation();
   document.body.dispatchEvent(new CustomEvent('darkmode:toggle', {
     detail: { checked: event.target.checked }
