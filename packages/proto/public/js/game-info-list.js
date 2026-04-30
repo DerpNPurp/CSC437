950fdf5fe7634e36ab9146b6fb4111e3e43124a4
DerpNPurp <vincedoanschool@gmail.com>
2026-04-30 01:02:41 -0700

    Lab 9 client side rendering

---
+++ packages/proto/public/js/game-info-list.js
@@ -0,0 +1,47 @@
+import { html, shadow } from "@unbndl/html";
+
+function renderPlatform(p) {
+  return html`<a href=${p.href}><svg class="icon"><use href="/icons/platforms.svg#${p.icon}"></use></svg> ${p.name}</a>`;
+}
+
+export class GameInfoListElement extends HTMLElement {
+  constructor() {
+    super();
+    shadow(this);
+  }
+
+  static observedAttributes = ["src"];
+
+  attributeChangedCallback(name, _, newValue) {
+    if (name === "src") {
+      this.hydrate(newValue).then((data) => {
+        const view = GameInfoListElement.render(data);
+        shadow(this).replace(view);
+      });
+    }
+  }
+
+  hydrate(src) {
+    return fetch(src)
+      .then((response) => {
+        if (response.status !== 200)
+          throw `HTTP Status ${response.status}`;
+        else return response.json();
+      })
+      .catch((error) => {
+        console.log(`Could not fetch ${src}:`, error);
+      });
+  }
+
+  static render(data) {
+    const { company, companyHref, genre, genreHref, genreIcon, rating, ratingHref, platforms } = data;
+    return html`
+      <game-info>
+        <span slot="company"><a href=${companyHref}>${company}</a></span>
+        <span slot="genre"><a href=${genreHref}><svg class="icon"><use href="/icons/genres.svg#${genreIcon}"></use></svg> ${genre}</a></span>
+        <span slot="rating"><a href=${ratingHref}>${rating}</a></span>
+        <span slot="platforms">${platforms.map(renderPlatform)}</span>
+      </game-info>
+    `;
+  }
+}
